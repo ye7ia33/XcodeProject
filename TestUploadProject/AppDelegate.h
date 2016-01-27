10fac92c88d5f3b4432bb2f493ab60a61e1fdbc1
ye7ia33 <yahia.eldow@gmail.com>
2016-01-27 11:33:22 +0200

    Initial Commit

---
+++ TestUploadProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestUploadProject
+//
+//  Created by Yahia on 1/27/16.
+//  Copyright © 2016 nichepharma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestUploadProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestUploadProject
+//
+//  Created by Yahia on 1/27/16.
+//  Copyright © 2016 nichepharma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
